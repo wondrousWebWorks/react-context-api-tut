48b0546537e8a2093b7155f3a4736331a76e3cfb
Johann Alberts <alberts.johann@gmail.com>
2021-07-12 21:44:04 +0100

    Change Booklist component to functional and add useContext

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,21 +1,18 @@
-import React, {Component} from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-  render() { 
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
-        <ul>
-          <li style={{ background: theme.ui }}>The Way of Kings</li>
-          <li style={{ background: theme.ui }}>The Name of the Wind</li>
-          <li style={{ background: theme.ui }}>The Final Empire</li>
-        </ul>
-      </div>
-    );
-  }
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+      <ul>
+        <li style={{ background: theme.ui }}>The Way of Kings</li>
+        <li style={{ background: theme.ui }}>The Name of the Wind</li>
+        <li style={{ background: theme.ui }}>The Final Empire</li>
+      </ul>
+    </div>
+  );
 }
  
 export default BookList;
